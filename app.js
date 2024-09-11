5470aaec9eb5ba4e38e76f1e020452a3f1c6368d
Hussain Al-Shammari <hussain@Hussains-MacBook-Air.local>
2024-09-11 12:28:47 -0400

    spotify auth

--- app.js
+++ app.js
@@ -20,7 +20,34 @@ app.get('/', (req, res) => {
     res.send('Welcome to Mood Muse! Click <a href="/login">here</a> to log in.');
   });
   
-  // Start the server
-  app.listen(port, () => {
+// Start the server
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+});
+
+const scopes = ['user-read-private', 'user-read-email', 'user-top-read'];
+
+app.get('/login', (req, res) => {
+  const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+  res.redirect(authorizeURL);
+});
+
+app.get('/callback', async (req, res) => {
+    const code = req.query.code || null;
+  
+    try {
+      const data = await spotifyApi.authorizationCodeGrant(code);
+      const accessToken = data.body['access_token'];
+      const refreshToken = data.body['refresh_token'];
+  
+      // Save the access token and refresh token
+      spotifyApi.setAccessToken(accessToken);
+      spotifyApi.setRefreshToken(refreshToken);
+  
+      res.send('Logged in! You can now use the app.');
+    } catch (err) {
+      console.error('Error in callback:', err);
+      res.send('Something went wrong!');
+    }
   });
+  
